66c2429d8e12917959ca6f3fa82ebd8dc77909d3
Mike Cooper <mythmon@gmail.com>
2014-08-16 01:25:20 -0700

    Wiggly dots!

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,104 @@
 (function() {
+  var TAU = Math.PI * 2;
+  var canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext('2d');
+
+  var atoms = [];
+
+  function random(a, b) {
+    if (b === undefined) {
+      b = a;
+      a = 0;
+    }
+    return Math.random() * (b - a) + a;
+  }
+
+  var types = ['fire', 'air', 'water', 'earth'];
+  for (var i = 0; i < 1000; i++) {
+    atoms.push({
+      // x: random(7, canvas.width - 7),
+      // y: random(7, canvas.height - 7),
+      x: canvas.width / 2,
+      y: canvas.height / 2,
+      vx: 0,
+      vy: 0,
+      // heat: random(1.0),
+      heat: 0.1,
+      r: 7,
+      type: types[random(4)|0],
+    });
+  }
+
+  function gameLoop() {
+    tick();
+    draw();
+    requestAnimationFrame(gameLoop);
+  }
+
+  function tick() {
+    var atom;
+    for (var i = 0; i < atoms.length; i++) {
+      atom = atoms[i];
+      // Heat
+      var ang = Math.random() * TAU;
+      var mag = Math.random() * atom.heat;
+      atom.vx += Math.cos(ang) * mag;
+      atom.vy += Math.sin(ang) * mag;
+
+      // Friction
+      // TODO: Should this create heat?
+      atom.vx *= 0.99;
+      atom.vy *= 0.99;
+
+      // Bounce
+      var radius = atom.r;
+      if (atom.x > canvas.width - radius) {
+        atom.vx = -Math.abs(atom.vx);
+        atom.x += atom.vx;
+      }
+      if (atom.x < radius) {
+        atom.vx = Math.abs(atom.vx);
+        atom.x += atom.vx;
+      }
+      if (atom.y > canvas.height - radius) {
+        atom.vy = -Math.abs(atom.vy);
+        atom.y += atom.vy;
+      }
+      if (atom.y < radius) {
+        atom.vy = Math.abs(atom.vy);
+        atom.y += atom.vy;
+      }
+
+      // Move
+      atom.x += atom.vx;
+      atom.y += atom.vy;
+    }
+  }
+
+  var typeColors = {
+    fire: '#ef3932',
+    water: '#1e26c9',
+    air: '#caccf1',
+    earth: '#965f32',
+  };
+
+  function draw() {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    var atom;
+    for (var i = 0; i < atoms.length; i++) {
+      atom = atoms[i];
+      ctx.save();
+      ctx.fillStyle = typeColors[atom.type],
+      ctx.translate(atom.x, atom.y);
+      ctx.beginPath();
+      ctx.arc(0, 0, atom.r, 0, TAU);
+      ctx.closePath();
+      ctx.fill();
+      ctx.restore();
+    }
+  }
+
+  gameLoop();
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -17,8 +17,9 @@ var uglify = require('gulp-uglify');
 var webserver = require('gulp-webserver');
 var zip = require('gulp-zip');
 
-gulp.task('watch', ['build'], function() {
-  gulp.watch('app/**/*', ['build']);
+gulp.task('watch', function() {
+  gulp.run('build-dev');
+  gulp.watch('app/**/*', ['build-dev']);
 });
 
 gulp.task('build', ['build.js', 'build.css', 'build.html']);
@@ -26,14 +27,14 @@ gulp.task('build', ['build.js', 'build.css', 'build.html']);
 gulp.task('build.html', function() {
   return gulp.src('app/index.html')
     .pipe(minifyHtml())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('min'));
 });
 
 gulp.task('build.css', function() {
   return gulp.src('app/**/*.css')
     .pipe(concat('app.css'))
     .pipe(minifyCss())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('min'));
 });
 
 gulp.task('build.js', function() {
@@ -44,31 +45,57 @@ gulp.task('build.js', function() {
      },
     }))
     .pipe(concat('app.js'))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('min'))
+});
+
+gulp.task('build-dev', ['build-dev.js', 'build-dev.css', 'build-dev.html']);
+
+gulp.task('build-dev.html', function() {
+  return gulp.src('app/index.html')
+    .pipe(gulp.dest('dev'));
+});
+
+gulp.task('build-dev.css', function() {
+  return gulp.src('app/**/*.css')
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest('dev'));
+});
+
+gulp.task('build-dev.js', function() {
+  return gulp.src('app/**/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('dev'))
 });
 
 gulp.task('package', ['build'], function() {
-  gulp.src('dist/**/*')
+  gulp.src('min/**/*')
     .pipe(zip('ewaf.zip'))
     .pipe(gulp.dest('.'));
 });
 
 gulp.task('clean', function() {
-  gulp.src(['./dist/**/*', './ewaf.zip'], {read: false})
+  gulp.src(['./dev/**/*', './min/**/*', './ewaf.zip'], {read: false})
     .pipe(rm());
 });
 
-gulp.task('server', ['build'], function() {
+function _serve() {
   var args = nomnom
    .option('port', {default: 8000})
    .option('host', {default: 'localhost'})
    .parse();
-  gulp.src('dist')
-    .pipe(webserver({
-      host: args.host,
-      port: args.port,
-    }));
+  return webserver({
+    host: args.host,
+    port: args.port,
+  });
+}
+
+gulp.task('server-dev', ['build-dev'], function() {
+  return gulp.src('dev').pipe(_serve());
 });
 
-gulp.task('dev', ['build', 'watch', 'server']);
-gulp.task('default', ['build']);
+gulp.task('server', ['build'], function() {
+  return gulp.src('min').pipe(_serve());
+});
+
+gulp.task('dev', ['build-dev', 'watch', 'server-dev']);
+gulp.task('default', ['build-dev']);
