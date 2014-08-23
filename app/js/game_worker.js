42f3fd600bc7ffbcd6a72def78f8cf477bad76f0
ekem <gorosher@gmail.com>
2014-08-23 16:25:06 -0700

    Initial commit

--- app/js/game_worker.js
+++ app/js/game_worker.js
@@ -1,10 +1,11 @@
-/* globals onmessage: true, postMessage: false */
-'use strict';
+'strict mode';
 var TAU = Math.PI * 2;
-var TIMESCALE = 0.1;
-var FRAMETIME = 30;
+var TIMESCALE = 0.08;
+var FRAMETIME = 20;
 var WIDTH;
 var HEIGHT;
+
+var mouse = {};
 
 onmessage = function(event) {
   var msg = event.data;
@@ -14,6 +15,9 @@ onmessage = function(event) {
       HEIGHT = msg.height;
       init();
       break;
+		case 'mouse':
+			mouse = msg.mouse;
+			break;
   }
 };
 
@@ -26,144 +30,136 @@ function random(a, b) {
 }
 
 var types = ['fire', 'air', 'water', 'earth'];
-var typeMaxBonds = {
-  air: 1,
-  earth: 2,
-  water: 3,
-  fire: 4,
-};
-function Atom() {
-  this.x = random(WIDTH);
-  this.y = random(HEIGHT);
-  this.vx = random(-1, 1);
-  this.vy = random(-1, 1);
-  this.heat = 0.1;
-  this.r = 7;
-  this.type = types[random(4)|0];
-  this.bonds = [];
-  this.max_bonds = typeMaxBonds[this.type];
-}
 
-function Bond(atom1, atom2) {
-  this.length = 10;
-  this.k = 0.001;
-  this.atom1 = atom1;
-  this.atom2 = atom2;
-  atom1.bonds.push(this);
-  atom2.bonds.push(this);
+function Atom() {
+  this.data = [0,0,0,255],
+	this.c = 0,
+  this.x = random(WIDTH),
+  this.y = random(HEIGHT),
+  this.vx = 0;
+  this.vy = 0;
+  this.heat = 0.1;
+  this.r = 4;
+  this.type = types[random(4)|0];
 }
 
 var atoms = [];
-var bonds = [];
 var lastRun;
 
 function init() {
-  var NUM_ATOMS = 400;
-  var i;
-  for (i = 0; i < NUM_ATOMS; i++) {
+  for (var i = 0; i < 500; i++) {
     atoms.push(new Atom());
   }
   lastRun = Date.now();
   setInterval(tick, FRAMETIME);
 }
 
-var E = 1;
-var K = 0.9;
-var F = 0.998;
-var E_MAX_DIST_SQ = Math.pow(E / 0.01, 2);
-
 function tick() {
-  var i, j;
-  var atom1, atom2, d, f, ang;
   var now = Date.now();
   var dt = (now - lastRun) * TIMESCALE;
   lastRun = now;
 
   var atom;
-  for (i = 0; i < atoms.length; i++) {
+  for (var i = 0; i < atoms.length; i++) {
     atom = atoms[i];
 
-    // Friction
-    atom.vx *= Math.pow(F, dt);
-    atom.vy *= Math.pow(F, dt);
+    // Heat
+    var ang = Math.random() * TAU;
+    var mag = Math.random() * atom.heat * dt;
+    atom.vx += Math.cos(ang) * mag;
+    atom.vy += Math.sin(ang) * mag;
 
-    // Move
-    atom.x += atom.vx * dt;
-    atom.y += atom.vy * dt;
+    // Friction
+    // TODO: Should this create heat?
+    atom.vx *= Math.pow(0.99, dt);
+    atom.vy *= Math.pow(0.99, dt);
 
     // Bounce
     var radius = atom.r;
     if (atom.x > WIDTH - radius) {
-      atom.vx = -Math.abs(atom.vx) * K;
+      atom.vx = -Math.abs(atom.vx);
       atom.x += atom.vx;
     }
     if (atom.x < radius) {
-      atom.vx = Math.abs(atom.vx) * K;
+      atom.vx = Math.abs(atom.vx);
       atom.x += atom.vx;
     }
     if (atom.y > HEIGHT - radius) {
-      atom.vy = -Math.abs(atom.vy) * K;
+      atom.vy = -Math.abs(atom.vy);
       atom.y += atom.vy;
     }
     if (atom.y < radius) {
-      atom.vy = Math.abs(atom.vy) * K;
+      atom.vy = Math.abs(atom.vy);
       atom.y += atom.vy;
     }
-  }
 
-  var bond;
-  for (i = 0; i < bonds.length; i++) {
-    bond = bonds[i];
-    atom1 = bond.atom1;
-    atom2 = bond.atom2;
-    d = Math.sqrt(distSq(atom1.x, atom1.y, atom2.x, atom2.y));
-    f = -(d - bond.length) * bond.k * dt;
-    ang = Math.atan2(atom1.y - atom2.y, atom1.x - atom2.x);
-    atom1.vx += Math.cos(ang) * f;
-    atom1.vy += Math.sin(ang) * f;
-    atom2.vx -= Math.cos(ang) * f;
-    atom2.vy -= Math.sin(ang) * f;
-  }
+    // Move
+    atom.x += atom.vx * dt;
+    atom.y += atom.vy * dt;
+		radius = 12;
+		if (mouse.x < atom.x + radius && mouse.x > atom.x-radius && mouse.y < atom.y + radius && mouse.y > atom.y-radius) {
+			//console.log('match');
+			atom.x = mouse.x;
+			atom.y = mouse.y;
+		}
+/*
+		else if (Math.abs(atom.x-mouse.x) < 50 && Math.abs(atom.y-mouse.y) < 50) {
+			if (mouse.x-atom.x > 0) {
+				atom.x--;
+			}
+			else {
+				atom.x++;
+			}
+		}
+		/*
+		if (Math.abs(atom.y-mouse.y) < 50) {
+			if (mouse.y-atom.y > 0) {
+				atom.y--;
+			}
+			else {
+				atom.y++;
+			}
+		}
+*/
+		atom.data[0] = Math.abs(atom.vx)*200;
+		atom.data[2] = Math.abs(atom.vy)*200;
 
-  for (i = 0; i < atoms.length - 1; i++) {
-    atom1 = atoms[i];
-    for (j = i + 1; j < atoms.length; j++) {
-      atom2 = atoms[j];
-      d = distSq(atom1.x, atom1.y, atom2.x, atom2.y);
-      if (d < E_MAX_DIST_SQ) {
-        if (d < 1) {
-          d = 1;
-        }
-        f = dt * E / d;
-        ang = Math.atan2(atom1.y - atom2.y, atom1.x - atom2.x);
-        atom1.vx += Math.cos(ang) * f;
-        atom1.vy += Math.sin(ang) * f;
-        atom2.vx -= Math.cos(ang) * f;
-        atom2.vy -= Math.sin(ang) * f;
-      }
-
-      if (d < 50 &&
-          Math.random() > 0.9 &&
-          atom1.bonds.length < atom1.max_bonds &&
-          atom2.bonds.length < atom2.max_bonds) {
-        bonds.push(new Bond(atom1, atom2));
-      }
+		if (i > 0 && checkBounds(i)) {
+			postMessage({
+				type: "hit",
+				atom: atom,
+			});
     }
+
+		if (i != 1 && checkBounds(i, 1)) {
+			atom.c++;
+			postMessage({
+				type: "hit",
+				atom: atom,
+				form: "grow",
+			});
+    }
+
   }
 
-  uploadState();
+  uploadAtoms();
 }
 
-function distSq(x1, y1, x2, y2) {
-  var dx = x1 - x2;
-  var dy = y1 - y2;
-  return dx * dx + dy * dy;
+function checkBounds() {
+	var atom0 = atoms[arguments[0]];
+	var atom1 = arguments[1] ? atoms[arguments[1]] : atoms[0]; 
+	radius = atom0.r;
+
+	if (atom0.x-radius < (atom1.x+radius) && atom0.x+radius > (atom1.x - radius) &&
+		atom0.y-radius < (atom1.y+radius) && atom0.y+radius > (atom1.y - radius)) {
+		return true;
+	}
+	return false;
 }
 
-function uploadState() {
+function uploadAtoms() {
   postMessage({
     type: 'gameTick',
     atoms: atoms,
-    bonds: bonds,
   });
 }
--- app/js/main.js
+++ app/js/main.js
@@ -1,82 +1,161 @@
-'use strict';
-var TAU = Math.PI * 2;
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
+(function() {
+  var TAU = Math.PI * 2;
+  var canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext('2d');
 
-var atoms = [];
-var bonds = [];
+  var data = ctx.createImageData(12, 12);
 
-function random(a, b) {
-  if (b === undefined) {
-    b = a;
-    a = 0;
+  var mouse = [];
+
+  var atoms = [];
+	var hits = [];
+
+	canvas.addEventListener('mousemove', function(e) {
+		mouse = getMousePos(canvas, e);
+		//console.log(mousePos);
+	}, false);
+
+	function getMousePos(canvas, e) {
+		var rect = canvas.getBoundingClientRect();
+		return {
+			x: (e.clientX-rect.left)/(rect.right-rect.left)*canvas.width,
+			y: (e.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height
+		}
+	};
+	
+
+	function utf16(text) {
+		if (text > 0xFFFF) {
+			text -= 0x10000;
+			return String.fromCharCode(0xD800 + (text >> 10), 0xDC00 + (text & 0x03FF));
+		} 
+		else { return String.fromCharCode(text); }
+	}
+
+  function random(a, b) {
+    if (b === undefined) {
+      b = a;
+      a = 0;
+    }
+    return Math.random() * (b - a) + a;
   }
-  return Math.random() * (b - a) + a;
-}
 
-var types = ['fire', 'air', 'water', 'earth'];
+  var types = ['fire', 'air', 'water', 'earth'];
 
-var sprites = {};
-var typeColors = {
-  fire: '#ef3932',
-  water: '#1e26c9',
-  air: '#caccf1',
-  earth: '#965f32',
-};
+  var sprites = {};
+  var typeColors = {
+    fire: '#ef3932',
+    water: '#1e26c9',
+    air: '#caccf1',
+    earth: '#669900',
+  };
 
-types.forEach(function(type) {
-  var spriteCanvas = document.createElement('canvas');
-  spriteCanvas.width = 14;
-  spriteCanvas.height = 14;
-  var spriteCtx = spriteCanvas.getContext('2d');
-  spriteCtx.fillStyle = typeColors[type];
-  spriteCtx.beginPath();
-  spriteCtx.arc(7, 7, 7, 0, TAU);
-  spriteCtx.closePath();
-  spriteCtx.fill();
-  sprites[type + '7'] = spriteCanvas;
-});
+  var typeChar = {
+		fire: 0x706b,
+		water: 0x6C34,
+		air: 0x632C,
+		earth: 0x5730,
+	};
 
-var gameWorker = new Worker('game_worker.js');
-gameWorker.addEventListener('message', function(event) {
-  var msg = event.data;
-  switch (msg.type) {
-    case 'gameTick':
-      atoms = msg.atoms;
-      bonds = msg.bonds;
-      if (!haveDrawn) {
-        haveDrawn = true;
-        draw();
+  types.forEach(function(type) {
+    var spriteCanvas = document.createElement('canvas');
+    spriteCanvas.width = 8;
+    spriteCanvas.height = 8;
+    var spriteCtx = spriteCanvas.getContext('2d');
+    spriteCtx.fillStyle = typeColors[type];
+    spriteCtx.beginPath();
+    spriteCtx.arc(4, 4, 4, 0, TAU);
+    spriteCtx.closePath();
+    spriteCtx.fill();
+    sprites[type + '4'] = spriteCanvas;
+  });
+
+  var gameWorker = new Worker('game_worker.js');
+  gameWorker.addEventListener('message', function(event) {
+    var msg = event.data;
+    switch (msg.type) {
+      case 'gameTick':
+        atoms = msg.atoms;
+        if (!haveDrawn) {
+          haveDrawn = true;
+          draw()
+        }
+        break;
+			case 'hit':
+				hits.push(msg.atom);
+
+				break;
+    }
+  });
+
+	function hit() {
+		
+	}
+
+  function drawText(text, size, color, x, y) {
+    ctx.fillStyle = color;
+    ctx.font = size;
+    ctx.fillText(text, x, y);
+  }
+
+	function toHex(c) {
+		var hex = c.toString(16);
+		return hex.length == 1 ? "0"+hex:hex;
+	}
+
+	function rgb(r,g,b) {
+		return "#"+toHex(r)+toHex(g)+toHex(b);
+	}
+
+  var haveDrawn = false;
+  function draw() {
+
+    ctx.fillStyle = '#101';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    var atom;
+		if (hits.length > 0) {
+			for (var i = 0; i < hits.length; i++) {
+				hit = hits[i];
+				drawText(utf16(typeChar[hit.type]), "52px sans-serif", rgb(i,
+							Math.trunc(random(1,244)), Math.trunc(random(1,244))),
+							hit.x, hit.y);
+				hits.pop();
+			}
+		}
+
+    for (var i = 0; i < atoms.length; i++) {
+      atom = atoms[i];
+
+      var color = "#FFF";
+      switch (i) {
+        case 0:
+          drawText("\u2620", "32px sans-serif", "#FFF", atoms[i].x, atoms[i].y);
+          break;
+        case 1:
+          drawText("\u262f", "32px sans-serif", "#AAFF88", atoms[i].x, atoms[i].y);
+          break;
       }
-      break;
+
+      for (var o=0; o < data.data.length; o+=4) {
+				for (var p=0; p < 4; p++){
+					data.data[o+p]=atom.data[p];
+				}
+      }
+
+      //ctx.putImageData(data, atom.x+4, atom.y);
+      drawText(utf16(typeChar[atom.type]), '12px sans-serif', color, atom.x+2, atom.y+12);
+			if (atom.c > 0) {
+				drawText(utf16(0x2741), (atom.c%124).toString() + 'px sans-serif', "#F"+(random(16)|0).toString(16)+"8", atom.x, atom.y);
+			}
+      var sprite = sprites[atom.type + atom.r];
+      //ctx.drawImage(sprite, atom.x - atom.r, atom.y - atom.r);
+      ctx.drawImage(sprite, atom.x - atom.r, atom.y - atom.r);
+
+    }
+		//console.log(mouse);
+		gameWorker.postMessage({type: 'mouse', mouse : mouse});
+    requestAnimationFrame(draw);
   }
-});
 
-var haveDrawn = false;
-function draw() {
-  var i;
-  ctx.fillStyle = '#000';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  var bond;
-  for (i = 0; i < bonds.length; i++) {
-    bond = bonds[i];
-    ctx.strokeStyle = '#fff';
-    ctx.strokeWidth = 2;
-    ctx.beginPath();
-    ctx.moveTo(bond.atom1.x, bond.atom1.y);
-    ctx.lineTo(bond.atom2.x, bond.atom2.y);
-    ctx.closePath();
-    ctx.stroke();
-  }
-
-  var atom;
-  for (i = 0; i < atoms.length; i++) {
-    atom = atoms[i];
-    var sprite = sprites[atom.type + atom.r];
-    ctx.drawImage(sprite, atom.x - atom.r, atom.y - atom.r);
-  }
-  requestAnimationFrame(draw);
-}
-
-gameWorker.postMessage({type: 'init', width: canvas.width, height: canvas.height});
+  gameWorker.postMessage({type: 'init', width: canvas.width, height: canvas.height});
+})();
